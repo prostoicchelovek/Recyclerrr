46fafaf8b4e19e2b14ab4796e530953517d40a6a
student_ait409 <prostoi.cchelovek@gmail.com>
2023-10-30 10:32:45 +0300

    recycler and card 1

--- app/src/main/java/com/example/recyclerrr/MainActivity.java
+++ app/src/main/java/com/example/recyclerrr/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<RecyclerrrItem> recyclerrrItems = new ArrayList<>();
+        recyclerrrItems.add(new RecyclerrrItem(R.drawable.baseline_emoji_emotions_24,"Happy","Life is fun!"));
+        recyclerrrItems.add(new RecyclerrrItem(R.drawable.baseline_sentiment_neutral_24,"Neutral","Life is bad("));
+        recyclerrrItems.add(new RecyclerrrItem(R.drawable.baseline_mood_bad_24,"Neutral","Life is life"));
     }
 }
